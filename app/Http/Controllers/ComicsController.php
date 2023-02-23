97077bd01fe30ceb67aa3dc8a38c34301b846abe
Mark-dev30 <fronterremarco@gmail.com>
2023-02-23 15:53:00 +0100

    completed index and show.php

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -80,7 +80,7 @@ class ComicsController extends Controller
      */
     public function create()
     {
-        //
+        return view('comics.create');
     }
 
     /**
@@ -102,7 +102,65 @@ class ComicsController extends Controller
      */
     public function show($id)
     {
-        //
+
+        $menu = [
+            'movies' => '/movies',
+            'tv' => '/tv',
+            'games' => '/games',
+            'collectibles' => '/collectibles',
+            'videos' => '/videos',
+            'fans' => '/fans',
+            'news' => '/news',
+            'shop' => '/shop'
+        ];
+
+        $menu_footer = [
+            "comics" => [
+                'Characters' => '/characters',
+                'Comics' => '/comics',
+                'Movies' => '/movies',
+                'Tv' => '/tv',
+                'Games' => '/games',
+                'Videos' => '/videos',
+                'News' => '/news',
+            ],
+            "shop" => [
+                'Shop DC' => '/shop-dc',
+                'Shop DC Collectibles' => '/shop-dc-collectibles',
+            ],
+            "dc" => [
+                'Term Of Use' => '/term-of-use',
+                'Privacy policy (New)' => '/privacy-policy',
+                'Ad Choices' => '/ad-choices',
+                'Advertising' => '/advertising',
+                'Jobs' => '/jobs',
+                'Subscriptions' => '/subscriptions',
+                'Talent Workshops' => '/talent-workshops',
+                'CPSC Certificates' => '/cpsc-certificates',
+                'Ratings' => '/ratings',
+                'Shop Help' => '/shop-help',
+                'Contact Us' => '/contact-us',
+
+            ],
+            "sites" => [
+                'DC' => '/dc',
+                'MAD Magazine' => '/mad-magazine',
+                'DC Kids' => '/dc-kids',
+                'DC Universe' => '/dc-universe',
+                'DC Power Visa' => '/dc-power-visa',
+            ],
+            "img_social" => [
+                'footer-facebook.png',
+                'footer-twitter.png',
+                'footer-youtube.png',
+                'footer-pinterest.png',
+                'footer-periscope.png'
+            ],
+        ];
+        $comic = Comic::findOrFail($id);
+
+        return view('comics.show', compact('comic', 'menu', 'menu_footer'));
+        abort(404);
     }
 
     /**
